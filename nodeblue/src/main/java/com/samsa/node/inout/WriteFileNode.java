d9f77e6818aeb9b6193bc72c462370c5c4cc3e53
jangwonseong <jangwonsung@gmail.com>
2024-12-13 10:37:12 +0900

    에러 제거

--- nodeblue/src/main/java/com/samsa/node/inout/WriteFileNode.java
+++ nodeblue/src/main/java/com/samsa/node/inout/WriteFileNode.java
@@ -1,31 +1,31 @@
-package com.samsa.node.inout;
+// package com.samsa.node.inout;
 
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import com.samsa.core.*;
+// import java.io.BufferedReader;
+// import java.io.File;
+// import java.io.FileReader;
+// import java.io.IOException;
+// import java.io.InputStreamReader;
+// import com.samsa.core.*;
 
-public class WriteFileNode extends Node {
+// public class WriteFileNode extends Node {
 
-    private String filePath = "./SAMSA/log/log.log";
-private String encoding = "UTF-8";
+// private String filePath = "./SAMSA/log/log.log";
+// private String encoding = "UTF-8";
 
-public void getlog() {
+// public void getlog() {
 
-try (BufferedReader reader = new BufferedReader()) {
-String line;
-if (line = reader.readLine() = !null) {
-
-}
-} catch (IOException e) {
-e.printStackTrace();
-}
-String line;
+// try (BufferedReader reader = new BufferedReader()) {
+// String line;
+// if (line = reader.readLine() = !null) {
 
 // }
+// } catch (IOException e) {
+// e.printStackTrace();
+// }
+// String line;
+
+// // }
 
 
 // public WriteFileNode(String filePath) {
